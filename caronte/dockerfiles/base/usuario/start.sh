7d00dc3c04b2a99b3533e6bfcd48b71f8e823de8
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-11-19 11:51:32 +0100

    actualizando

---
+++ caronte/dockerfiles/base/usuario/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+configurar-ssh() {
+    sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
+    sed -i 's/#PermitRootLogin./PermitRootLogin no/' /etc/ssh/sshd_config
+    service ssh restart
+    mkdir /home/$USUARIO/.ssh
+    cp /root/admin/common/ /home/$USUARIO/.ssh/
+    #exec /usr/sbin/sshd -D &
+}
