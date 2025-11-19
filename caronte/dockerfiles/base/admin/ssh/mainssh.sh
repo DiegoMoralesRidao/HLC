bfb3a8a1023739b1d902490aec096dd158c9cc50
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-11-19 12:11:24 +0100

    Configuración ssh y red

--- caronte/dockerfiles/base/admin/ssh/mainssh.sh
+++ caronte/dockerfiles/base/admin/ssh/mainssh.sh
@@ -4,10 +4,9 @@ configurar_ssh() {
     # Configurar SSH para permitir el acceso root
     sed -i 's/PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
     sed -i 's/#Port.*/Port '$Port_SSH'/' /etc/ssh/sshd_config
-    if [ ! d /home/$USUARIO/.ssh ]; then
     mkdir -p /home/$USUARIO/.ssh
     cat /root/datos/id_rsa.pub >> /home/$USUARIO/.ssh/authorized_keys
-   fi
+    
 
     service ssh restart
 }
